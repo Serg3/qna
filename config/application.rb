038e81f2908e1063fbef7581d8cbfafef45def38
Serg3 <jaguar.sergey@gmail.com>
2018-04-21 14:16:14 +0300

    add gem slim, setup test's generator

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,16 @@ module Qna
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_spec: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_spec: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
