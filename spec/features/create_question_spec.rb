7c22fdb0246ab5fb14a8dc6537f778ea671f0d60
Serg3 <jaguar.sergey@gmail.com>
2018-04-24 20:29:43 +0300

    acceptance test for ask question with incorrect data

--- spec/features/create_question_spec.rb
+++ spec/features/create_question_spec.rb
@@ -11,8 +11,21 @@ feature 'Create question', %q{
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: "question's text"
     click_on 'Create'
-    
+
     expect(page).to have_content 'Your question successfully created.'
+    expect(page).to have_content 'Test question'
+    expect(page).to have_content "question's text"
+  end
+
+  scenario 'User creates question with errors' do
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: ''
+    click_on 'Create'
+
+    expect(page).to have_content "Title can't be blank"
+    expect(page).to have_content "Body can't be blank"
   end
 
 end
