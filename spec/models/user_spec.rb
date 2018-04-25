9284ac7d13d3a52293847c76f3712bb6118da31b
Serg3 <jaguar.sergey@gmail.com>
2018-04-25 21:06:44 +0300

    add test for User#author_of?(resource)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,17 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many(:questions) }
   it { should have_many(:answers) }
+
+  describe "#author_of?(resource)" do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:question) { create(:question, user: user1) }
+
+    it "user is an author of question" do
+      expect(user1.author_of?(question)).to eq true
+    end
+    it "user is not an author of question" do
+      expect(user2.author_of?(question)).to eq false
+    end
+  end
 end
