ca92547c09a57b5d0b76ed33b8c4fe074c7838e8
Serg3 <jaguar.sergey@gmail.com>
2018-05-07 20:14:56 +0300

    add #set_best & #by_best to Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,13 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: :true
+
+  scope :by_best, -> { order(best: :desc, created_at: :desc) }
+
+  def set_best
+    current_best = question.answers.find_by(best: true)
+
+    current_best.update!(best: false) if current_best
+    update!(best: true)
+  end
 end
