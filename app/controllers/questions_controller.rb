3bda0d0d638bb7a348eb8a76e7e2143a5ae8ec22
Serg3 <jaguar.sergey@gmail.com>
2018-05-28 17:51:41 +0300

    edit question's controller, views, tests for gem responders

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,53 +3,37 @@ class QuestionsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create, :update, :destroy]
   before_action :load_question, only: [:show, :update, :destroy]
+  before_action :build_answer, only: :show
   after_action :publish_question, only: [:create]
 
+  respond_to :js
+
   def index
-    @questions = Question.all
+    respond_with(@questions = Question.all)
   end
 
   def show
     @answers = @question.answers.by_best
-    @answer = Answer.new
-    @answer.attachments.build
+    respond_with @question
   end
 
   def new
-    @question = current_user.questions.build
-    @question.attachments.build
+    respond_with(@question = current_user.questions.build)
   end
 
   def create
-    @question = current_user.questions.build(question_params)
-
-    if @question.save
-      flash[:notice] = 'Your question successfully created.'
-      redirect_to @question
-    else
-      render :new
-    end
+    respond_with(@question = current_user.questions.create(question_params))
   end
 
   def update
     if current_user.author_of?(@question)
       @question.update(question_params)
-      flash[:notice] = 'Your question successfully updated.'
-    else
-      flash[:alert] = "You can not edit another user's question!"
-      redirect_to @question
+      respond_with @question
     end
   end
 
   def destroy
-    if current_user.author_of?(@question)
-      @question.destroy
-      flash[:notice] = 'Your question successfully deleted.'
-    else
-      flash[:alert] = "You can not delete another user's question!"
-    end
-
-    redirect_to questions_path
+    respond_with(@question.destroy) if current_user.author_of?(@question)
   end
 
   private
@@ -58,13 +42,17 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def build_answer
+    @answer = @question.answers.build
+  end
+
   def question_params
     params.require(:question).permit(:title, :body, attachments_attributes: [:file, :id, :_destroy])
   end
 
   def publish_question
     return if @question.errors.any?
-    
+
     ActionCable.server.broadcast('questions', question: @question)
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -25,10 +25,6 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to eq question
     end
 
-    it 'builds new attachment for answer' do
-      expect(assigns(:answer).attachments.first).to be_a_new(Attachment)
-    end
-
     it 'renders show view' do
       expect(response).to render_template :show
     end
@@ -39,10 +35,6 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'assigns a new Question to @question' do
       expect(assigns(:question)).to be_a_new(Question)
-    end
-
-    it 'builds new attachment for question' do
-      expect(assigns(:question).attachments.first).to be_a_new(Attachment)
     end
 
     it 'renders new view' do
@@ -134,12 +126,6 @@ RSpec.describe QuestionsController, type: :controller do
 
       it 'delete question' do
         expect { delete :destroy, params: { id: @question2 } }.to_not change(Question, :count)
-      end
-
-      it 'redirect to index view' do
-        delete :destroy, params: { id: @question2 }
-
-        expect(response).to redirect_to questions_path
       end
     end
   end
