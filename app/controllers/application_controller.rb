db9aeecb6c3c7f66b5dc2a6c11386a189601982a
Serg3 <jaguar.sergey@gmail.com>
2018-06-04 19:36:38 +0300

    add response formats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,11 @@ class ApplicationController < ActionController::Base
   before_action :check_real_email
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to root_path, alert: exception.message }
+      format.js   { head :forbidden, content_type: 'text/html' }
+    end
   end
 
   private
