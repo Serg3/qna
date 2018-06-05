1a59e5238a446837dc4b80569d5e12674dfbccf5
Serg3 <jaguar.sergey@gmail.com>
2018-06-05 20:34:37 +0300

    edit response formats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
-      format.json { head :forbidden, content_type: 'text/html' }
+      format.json { head :forbidden }
       format.html { redirect_to root_path, alert: exception.message }
-      format.js   { head :forbidden, content_type: 'text/html' }
+      format.js   { head :forbidden }
     end
   end
 
