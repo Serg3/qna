03c49dc157f44cd7b246da5a5adce7092af8ba47
Serg3 <jaguar.sergey@gmail.com>
2018-06-16 15:17:41 +0300

    change test's type for indexes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,19 +71,19 @@ RSpec.configure do |config|
 
   config.before :each do |example|
     # Configure and start Sphinx for request specs
-    if example.metadata[:type] == :request
+    if example.metadata[:type] == :feature
       ThinkingSphinx::Test.init
       ThinkingSphinx::Test.start index: false
     end
 
     # Disable real-time callbacks if Sphinx isn't running
     ThinkingSphinx::Configuration.instance.settings['real_time_callbacks'] =
-        (example.metadata[:type] == :request)
+        (example.metadata[:type] == :feature)
   end
 
   config.after(:each) do |example|
     # Stop Sphinx and clear out data after request specs
-    if example.metadata[:type] == :request
+    if example.metadata[:type] == :feature
       ThinkingSphinx::Test.stop
       ThinkingSphinx::Test.clear
     end
