35f74d7035f97b3e84b5a92fe9ee76a19cf09856
Serg3 <jaguar.sergey@gmail.com>
2018-06-25 21:50:55 +0300

    add cache for question, answers, comments, attachments

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,5 @@
 class Attachment < ApplicationRecord
-  belongs_to :attachable, polymorphic: true
+  belongs_to :attachable, polymorphic: true, touch: true
 
   mount_uploader :file, FileUploader
 end
